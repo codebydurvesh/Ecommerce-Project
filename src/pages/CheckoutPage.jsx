b4d0ebe4181fec0e07cd41bf376001dd6f1d3765
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-10-22 09:36:22 +0530

    Made the CheckoutHeader into the Components in linked it

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,6 +1,7 @@
-import './checkout-header.css'
+import CheckoutHeader from "../Components/CheckoutHeader";
 import "./CheckoutPage.css"
 import { Link } from 'react-router-dom';
+
 
 function CheckoutPage(){
     return(
@@ -8,25 +9,7 @@ function CheckoutPage(){
 
             <title>Checkout</title>
 
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                    <Link to="/">
-                        <img className="logo" src="images/logo.png" />
-                        <img className="mobile-logo" src="images/mobile-logo.png" />
-                    </Link>
-                    </div>
-
-                    <div className="checkout-header-middle-section">
-                    Checkout (<Link className="return-to-home-link"
-                        to="/">3 items</Link>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                    <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <CheckoutHeader />
 
             <div className="checkout-page">
             <div className="page-title">Review your order</div>
