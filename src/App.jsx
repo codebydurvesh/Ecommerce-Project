4abe175009a2e7190031ab45ecb5ef74ee99daba
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-10-22 09:05:47 +0530

    Add the orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import CheckoutPage from './pages/CheckoutPage.jsx'
+import OrdersPage from './pages/OrdersPage.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='checkout' element={<CheckoutPage />} />
+        <Route path='orders' element={<OrdersPage />} />
       </Routes>
     </>
   )
