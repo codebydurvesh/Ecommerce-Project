7aead000f9a68ebf3d1f26514059b106ffe8a193
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-10-22 16:31:07 +0530

    Generate the HTML for the orders page

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,37 +1,33 @@
-import {Routes, Route} from 'react-router-dom'
-import HomePage from './pages/Home/HomePage.jsx'
-import CheckoutPage from './pages/Checkout/CheckoutPage.jsx'
-import OrdersPage from './pages/Orders/OrdersPage.jsx'
-import TrackingPage from './pages/Tracking/TrackingPage.jsx'
-import ErrorPage from './pages/ErrorPage/ErrorPage.jsx'
-import './App.css'
-import { useState , useEffect } from 'react'
-import axios from 'axios'
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage.jsx";
+import CheckoutPage from "./pages/Checkout/CheckoutPage.jsx";
+import OrdersPage from "./pages/Orders/OrdersPage.jsx";
+import TrackingPage from "./pages/Tracking/TrackingPage.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        setCart(response.data);
-      })
-  }, [])
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage cart={cart} />} />
-        <Route path='checkout' element={<CheckoutPage cart={cart} />} />
-        <Route path='orders' element={<OrdersPage />} />
-        <Route path='tracking' element={<TrackingPage />} />
-        <Route path='*' element={<ErrorPage />} />
-
+        <Route path="/" element={<HomePage cart={cart} />} />
+        <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+        <Route path="orders" element={<OrdersPage cart={cart} />} />
+        <Route path="tracking" element={<TrackingPage cart={cart} />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
