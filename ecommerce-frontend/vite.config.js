d7d3662a107dfdc2f69321dec51d7a8929d1b23a
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-10-25 10:08:45 +0530

    Changed the target url

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000'
+      "/api": {
+        target: "https://ecommerce-project-backend-n78t.onrender.com",
       },
-      '/images': {
-        target: 'http://localhost:3000'
-      }
-    }
-  }
-})
+      "/images": {
+        target: "https://ecommerce-project-backend-n78t.onrender.com",
+      },
+    },
+  },
+});
