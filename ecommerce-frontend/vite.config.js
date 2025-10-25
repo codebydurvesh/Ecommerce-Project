6a1ae2b3f60a8f45ddc42178f0b44434e099ecbb
codebydurvesh <durvesh.gaikwad08@gmail.com>
2025-10-25 12:29:07 +0530

    Changes in the vite config file to connect to the public backend we created

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -8,9 +8,13 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://ecommerce-project-backend-n78t.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
       "/images": {
         target: "https://ecommerce-project-backend-n78t.onrender.com",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
